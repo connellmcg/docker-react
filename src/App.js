6a9b3af7cead6e17467046479b8821aab92a469c
connellmcg <connell@DESKTOP-8JDTE31.localdomain>
2020-09-11 16:14:03 +0100

    adding to feature branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi There. You Legend :)
+          <p>Are you ready for the weekend?</p>
         </p>
         <a
           className="App-link"
